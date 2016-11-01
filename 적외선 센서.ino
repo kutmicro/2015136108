39e22fba6bda07d2c24e8bbbff5df98baa431d88
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-01 11:35:32 +0900

    Create 적외선 센서.ino

---
+++ 적외선 센서.ino
@@ -0,0 +1,13 @@
+int analogPin = 0;
+
+void setup(){
+  Serial.begin(9600);     
+}
+void loop(){
+    int val;
+    int volt = analogRead(analogPin);
+    val = (6762/(volt-9))-4;
+    Serial.println(val);
+    delay(100);     
+}
+//거리측정
