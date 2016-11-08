7a214242e40b15430e484c5900652ed5110c8391
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-08 11:33:30 +0900

    Update 적외선 센서.ino

--- 적외선 센서.ino
+++ 적외선 센서.ino
@@ -8,6 +8,14 @@ void loop(){
     int volt = analogRead(analogPin);
     val = (6762/(volt-9))-4;
     Serial.println(val);
+    if(val <= 45)
+    {
+      // 후진 후 정지
+      if(val == 45)
+      {
+        
+      }
+    }
     delay(100);     
 }
 //거리측정
