0c8b724b5cc68fe065a370ba4dad522becc152a7
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-10 12:05:21 +0900

    Update 서보모터 소스코드.ino

--- 서보모터 소스코드.ino
+++ 서보모터 소스코드.ino
@@ -5,13 +5,13 @@
     #include "Servo.h" //서보 라이브러리
  
     Servo myservo; //서보객체
-    int blueTx=2;   //Tx (블투 보내는핀 설정)
-    int blueRx=3;   //Rx (블투 받는핀 설정)
+    int blueTx=14;   //Tx (블투 보내는핀 설정)
+    int blueRx=15;   //Rx (블투 받는핀 설정)
     SoftwareSerial mySerial(blueTx, blueRx);  //시리얼 통신을 위한 객체선언
  
     void setup() {
-      myservo.attach(12);   //서보 시그널 핀설정
-      myservo.write(0);     //서보 초기각도 90도 설정
+      myservo.attach(9);   //서보 시그널 핀설정
+      myservo.write(90);     //서보 초기각도 90도 설정
       mySerial.begin(9600); //블루투스 시리얼 개방
     }
  
