7bc2f179e0d2a74045a102267548f2ada9c26da9
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-22 11:42:03 +0900

    Update 블루투스모듈.ino

--- 블루투스모듈.ino
+++ 블루투스모듈.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 SoftwareSerial
-BTSerial(2, 3); //아두이노 D2에 TXD, D3에 RXD를 연결한다는  
+BTSerial(14, 15); //아두이노 D2에 TXD, D3에 RXD를 연결한다는  
 void setup()  
 {  
   Serial.begin(9600);
