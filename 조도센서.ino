10d7792dc6ae53391f6714f65e6a165a6b12d766
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-09 23:48:40 +0900

    Update 조도센서.ino

--- 조도센서.ino
+++ 조도센서.ino
@@ -1,13 +1,10 @@
 // before change
 #define POT_PIN A0 
 #define LED_PIN 22 
-int blueTx=14;   //Tx (블투 보내는핀 설정) 
-int blueRx=15;   //Rx (블투 받는핀 설정) 
-SoftwareSerial mySerial(blueTx, blueRx); 
-
 
 void setup()
 {
+  Serial3.begin(9600_;
   pinMode(LED_PIN, OUTPUT);
   
 }
@@ -16,9 +13,9 @@ void loop()
   int val = 0;
   val = analogRead(POT_PIN); 
   
-  if(Serial.available())
+  if(Serial3.available())
   {
-    char data = Serial.read();
+    char data = Serial3.read();
     if(data == o) // if turn on the switch
     {
       //조도센서 값이 100미만이면 LED를 켠다
