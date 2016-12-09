855b0f94516f2f121055bfaf1fc2b09d711d0a73
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-10 01:03:28 +0900

    Update 초음파 센서.ino

--- 초음파 센서.ino
+++ 초음파 센서.ino
@@ -1,29 +1,16 @@
+/* not bluetooth
 #include <SoftwareSerial.h>
 
 int distance;
-
 int trig = 46;
-
 int echo = 44;
-
-
-
 
 void setup() {
 
-  
-
   Serial.begin(9600);
-
-  Serial3.begin(9600);
-
-
-
-
+  
   pinMode(trig, OUTPUT);
-
   pinMode(echo, INPUT);
-
 }
 
 
@@ -31,36 +18,64 @@ void setup() {
 
 void loop() {
 
-
-
-
   digitalWrite(trig,HIGH);
-
   delayMicroseconds(10);
-
   digitalWrite(trig,LOW);
-
-
 
 
   distance = pulseIn(echo,HIGH) / 58;
 
 
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  delay(1000);
+
+}
+
+*/
+
+
+
+
+
+
+#include <SoftwareSerial.h>
+
+int distance;
+int trig = 46;
+int echo = 44;
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial3.begin(9600);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+
+
+
+void loop() {
+
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+
+  distance = pulseIn(echo,HIGH) / 58;
 
 
   Serial.print("Distance = ");
-
   Serial.print(distance);
-
   Serial.println(" cm");
-
-
-
 
   Serial3.write(distance);
 
   
-
   delay(1000);
 
 }
